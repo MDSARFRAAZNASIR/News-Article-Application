417f6d13b46f30dbc6fe77ed13b5efc51ba42222
MDSARFRAAZNASIR <mdsarfraaznasir@gmail.com>
2025-05-24 11:43:03 +0530

    Create user model

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
+
 .then(()=>{
     console.log("Database is connected");
 }).catch((err)=>{
@@ -12,4 +13,4 @@ mongoose.connect(process.env.MONGO_URL)
 const app = express();
 app.listen(3000, () => {
     console.log("Server is running in port 3000")
-})
+});
