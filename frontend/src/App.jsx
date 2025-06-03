dd02fa9e566cd47fd5e9df99f16a63b375ae1b93
MDSARFRAAZNASIR <mdsarfraaznasir@gmail.com>
2025-06-03 13:02:53 +0530

    signup page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./index.css"
 // import { Button } from './components/ui/ui/button'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Ghost } from "lucide-react";
@@ -7,17 +8,19 @@ import LogInForm from "./auth/forms/LogInForm";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Hader from "./components/Hader";
+import Sign from "./auth/forms/Sign";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Hader />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-
         <Route path="/sign-in" element={<SignUpForm />} />
         <Route path="/Log-in" element={<LogInForm />} />
+        <Route path="/sign" element={<Sign/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/auth/forms/Sign.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+
+const Sign = () => {
+  return (
+    <div>
+         <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl sm:max-w-5xl max-auto flex-col md:flex-row md:items-center gap-5'>
+      
+        <div className="flex-1">
+          <Link to={"/"} className='font-bold text-2xl sm:text-4xl flex flex-wrap'>
+          <span>Morning</span>
+          </Link>
+        </div>
+     
+      </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Sign
